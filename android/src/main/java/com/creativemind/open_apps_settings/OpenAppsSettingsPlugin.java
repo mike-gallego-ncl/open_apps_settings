703c08ce33b030e2e20b17dac459bee92c1f4180
mike-gallego-ncl <137790936+mike-gallego-ncl@users.noreply.github.com>
2025-06-30 10:09:51 -0400

    Update OpenAppsSettingsPlugin.java

--- android/src/main/java/com/creativemind/open_apps_settings/OpenAppsSettingsPlugin.java
+++ android/src/main/java/com/creativemind/open_apps_settings/OpenAppsSettingsPlugin.java
@@ -17,7 +17,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** OpenAppsSettingsPlugin */
 public class OpenAppsSettingsPlugin implements FlutterPlugin, MethodCallHandler, PluginRegistry.ActivityResultListener, ActivityAware {
